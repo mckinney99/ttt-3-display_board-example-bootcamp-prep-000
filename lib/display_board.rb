7e8cb5a8de96c525ba654d08b322a100ba9d447e
Eric McKinney <mckinney99@gmail.com>
2017-10-10 15:02:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@ def display_board
   row1 = " ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "
   row2 = "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"
 
-  puts row1 
-  puts row2 
-  puts row1 
+  puts row1
+  puts row2
+  puts row1
 end
